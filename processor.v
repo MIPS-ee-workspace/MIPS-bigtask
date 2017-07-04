1ad29bb1a6405fcacb66d66f075c7478c91a337b
nickel-trace <nieyz15@mails.tsinghua.edu.cn>
2017-07-04 20:59:55 +0800

    初步改成了流水线型

--- processor.v
+++ processor.v
@@ -44,9 +44,11 @@ end
 
 //
 
-//PC, core_hazard
+
+//IF
+	//PC,core_hazard
 reg[31:0] PC_next;
-wire[31:0] PC4;
+wire[31:0] PC4_IF;
 
 always@(negedge reset or posedge clk) begin
 	if(~reset)begin
@@ -61,108 +63,22 @@ always@(negedge reset or posedge clk) begin
 	end
 end
 
-assign PC4=PC+4;
-//
+assign PC4_IF=PC+4;
+	//
 
-//Instruction Memory
-wire[31:0] Instruction;
-ROM instruction_memory(PC[30:0],Instruction,PC_overflow);
+	//Instruction Memory
+wire[31:0] Instruction_IF;
+ROM instruction_memory(PC[30:0],Instruction_IF,PC_overflow);
+	//
 
-wire[4:0] Rs;
-assign Rs[4:0]=Instruction[25:21];
-
-wire[4:0] Rt;
-assign Rt[4:0]=Instruction[20:16];
-
-wire[4:0] Rd;
-assign Rd[4:0]=Instruction[15:11];
-
-wire[4:0] Shamt;
-assign Shamt[4:0]=Instruction[10:6];
-
-wire[15:0] Imm16;
-assign Imm16[15:0]=Instruction[15:0];
-
+	//PC_next	need big change
 wire[31:0] JT;
-assign JT={PC4[31:28],Instruction[25:0],2'b00};
-//
+assign JT={PC4_IF[31:28],Instruction_IF[25:0],2'b00};
 
-//control, Inte,Exce,zero,nega
-wire[1:0] PCSrc;
-wire[1:0] RegDst;
-wire[1:0] MemToReg;
-wire[5:0] ALUFun;
-wire RegWr,ALUSrc1,ALUSrc2,Sign,MemWr,MemRd,EXTOp,LUOp;
-
-CPU_Control control(Instruction[31:26],Instruction[5:0],Interrupt,Exception,PCSrc,RegDst,RegWr,ALUSrc1,ALUSrc2,ALUFun,Sign,MemWr,MemRd,MemToReg,EXTOp,LUOp);
-
-//
-
-//Register File
-reg[4:0] AddrC;
-wire[31:0] DatabusA;
-wire[31:0] DatabusB;
-reg[31:0] DatabusC;
-
-always@(*)
-begin
-	case(RegDst)
-		2'b01:AddrC<=Rt;
-		2'b10:AddrC<=5'd31;
-		2'b11:AddrC<=5'd26;
-		default:AddrC<=Rd;
-	endcase
-end
-
-RegFile register_file(reset,clk,Rs,DatabusA,Rt,DatabusB,RegWr,AddrC,DatabusC, uart_send);
-//
-
-//ALU
-wire[31:0] ConBA;
-wire[31:0] ALUOut;
-wire[31:0] ALUIn1;
-wire[31:0] ALUIn2;
-wire[31:0] Imm32;
-wire[31:0] ImmExt;
-wire[31:0] Offset;
-
-assign ALUIn1=(ALUSrc1)?{27'd0,Shamt[4:0]}:DatabusA;
-assign ImmExt=(EXTOp && Imm16[15])?{16'hffff,Imm16}:{16'h0000,Imm16};
-assign Imm32=(LUOp)?{Imm16,16'h0000}:ImmExt;
-assign ALUIn2=(ALUSrc2)?Imm32:DatabusB;
-
-ALU ALU_mod(ALUIn1,ALUIn2,ALUFun,Sign,ALUOut,ALU_overflow);
-
-assign Offset={ImmExt[29:0],2'b00};
-assign ConBA=Offset+PC4;
-//
-
-//Data Memory
-wire[31:0] rdata1;
-wire[31:0] rdata2;
-wire[31:0] ReadData;
-wire MemRd1,MemRd2,MemWr1,MemWr2;
-
-assign {MemRd2,MemRd1,MemWr2,MemWr1}=(ALUOut < 32'h40000000)?{MemRd,1'b0,MemWr,1'b0}:{1'b0,MemRd,1'b0,MemWr};
-
-Peripheral peripheral(reset,sysclk,clk,MemRd1,MemWr1,ALUOut,DatabusB,rdata1, led[7:0],switch,digi,timer, UART_RX,UART_TX,uart_send);
-DataMem data_memory(reset,clk,MemRd2,MemWr2,ALUOut,DatabusB,rdata2);
-
-assign ReadData= rdata1 | rdata2;
-
-always@(*)
-begin
-	case(MemToReg)
-		2'b00:DatabusC<=ALUOut;
-		2'b01:DatabusC<=ReadData;
-		default:DatabusC<=PC4;
-	endcase
-end
-//
-
-//PC_next
 wire[31:0] ConBA2;
-assign ConBA2=(ALUOut[0])?ConBA:PC4;
+wire[31:0] DatabusA;
+
+assign ConBA2=(ALUOut[0])?ConBA:PC4_IF;
 
 always@(*)
 begin
@@ -170,9 +86,188 @@ begin
 		2'b01:PC_next<=ConBA2;
 		2'b10:PC_next<=JT;
 		2'b11:PC_next<=DatabusA;
-		default:PC_next<=PC4;
+		default:PC_next<=PC4_IF;
 	endcase
 end
-//
+	//
+
+//	IF/ID
+reg[31:0] Instruction_1;
+reg[31:0] PC4_1;
+always@(negedge reset or posedge clk) begin
+	if(~reset)begin
+		Instruction_1 <= 32'd0;
+		PC4_1 <= 32'd0;
+	end
+	else begin
+		Instruction_1 <= Instruction_IF;
+		PC4_1 <= PC4_IF;
+	end
+end
+
+//ID
+wire[4:0] Rt_ID;
+assign Rt_ID[4:0]=Instruction_1[20:16];
+wire[4:0] Rd_ID;
+assign Rd_ID[4:0]=Instruction_1[15:11];
+wire[4:0] Shamt;
+assign Shamt[4:0]=Instruction_1[10:6];
+wire[15:0] Imm16;
+assign Imm16[15:0]=Instruction_1[15:0];
+
+	//control, Inte,Exce
+wire[1:0] PCSrc;
+wire[1:0] RegDst_ID;
+wire[1:0] MemToReg_ID;
+wire[5:0] ALUFun_ID;
+wire RegWr_ID,ALUSrc1,ALUSrc2,Sign_ID,MemWr_ID,MemRd_ID,EXTOp,LUOp;
+
+CPU_Control control(Instruction_1[31:26],Instruction_1[5:0],Interrupt,Exception,PCSrc,RegDst_ID,RegWr_ID,ALUSrc1,ALUSrc2,ALUFun_ID,Sign_ID,MemWr_ID,MemRd_ID,MemToReg_ID,EXTOp,LUOp);
+
+	//
+
+	//Register File
+reg[4:0] AddrC;
+reg[31:0] DatabusC;
+reg RegWr_3;
+wire[31:0] DatabusB_ID;
+
+RegFile register_file(reset,clk,Instruction_1[25:21],DatabusA,Rt_ID,DatabusB_ID,RegWr_3,AddrC,DatabusC, uart_send);//change RegWr
+	//
+
+wire[31:0] ALUIn1_ID;
+wire[31:0] ALUIn2_ID;
+wire[31:0] Imm32;
+wire[31:0] ImmExt_ID;
+assign ALUIn1_ID=(ALUSrc1)?{27'd0,Shamt[4:0]}:DatabusA;
+assign ImmExt_ID=(EXTOp && Imm16[15])?{16'hffff,Imm16}:{16'h0000,Imm16};
+assign Imm32=(LUOp)?{Imm16,16'h0000}:ImmExt_ID;
+assign ALUIn2_ID=(ALUSrc2)?Imm32:DatabusB_ID;
+
+//	ID/EX
+reg[31:0] PC4_2;
+reg[31:0] ALUIn1_2;
+reg[31:0] ALUIn2_2;
+reg[31:0] DatabusB_2;
+reg[31:0] ImmExt_2;
+reg[5:0] ALUFun_2;
+reg[1:0] RegDst_2;
+reg[1:0] MemToReg_2;
+reg[4:0] Rt_2;
+reg[4:0] Rd_2;
+reg Sign_2,RegWr_2,MemWr_2,MemRd_2;
+always@(negedge reset or posedge clk) begin
+	if(~reset)begin
+		PC4_2 <= 32'd0;
+		ALUIn1_2 <= 32'd0;
+		ALUIn2_2 <= 32'd0;
+		DatabusB_2 <= 32'd0;
+		ImmExt_2 <= 32'd0;
+		Rt_2 <= 5'd0;
+		Rd_2 <= 5'd0;
+		ALUFun_2 <= 6'd0;
+		RegDst_2 <= 2'd0;
+		MemToReg_2 <= 2'd0;
+		Sign_2 <= 1'd0;
+		RegWr_2 <= 1'd0;
+		MemWr_2 <= 1'd0;
+		MemRd_2 <= 1'd0;
+	end
+	else begin
+		PC4_2 <= PC4_1;
+		ALUIn1_2 <= ALUIn1_ID;
+		ALUIn2_2 <= ALUIn2_ID;
+		DatabusB_2 <= DatabusB_ID;
+		ImmExt_2 <= ImmExt_ID;
+		Rt_2 <= Rt_ID;
+		Rd_2 <= Rd_ID;
+		ALUFun_2 <= ALUFun_ID;
+		RegDst_2 <= RegDst_ID;
+		MemToReg_2 <= MemToReg_ID;
+		Sign_2 <= Sign_ID;
+		RegWr_2 <= RegWr_ID;
+		MemWr_2 <= MemWr_ID;
+		MemRd_2 <= MemRd_ID;
+	end
+end
+
+//EX
+	//ALU
+wire[31:0] ALUOut_EX;
+ALU ALU_mod(ALUIn1_2,ALUIn2_2,ALUFun_2,Sign_2,ALUOut_EX,ALU_overflow);
+
+wire[31:0] Offset;
+wire[31:0] ConBA;
+assign Offset={ImmExt_2[29:0],2'b00};
+assign ConBA=Offset+PC4_2;
+	//
+
+//	EX/MEM
+reg[31:0] PC4_3;
+reg[31:0] ALUOut_3;
+reg[31:0] DatabusB_3;
+reg[1:0] RegDst_3;
+reg[1:0] MemToReg_3;
+reg[4:0] Rt_3;
+reg[4:0] Rd_3;
+reg MemWr_3,MemRd_3;
+always@(negedge reset or posedge clk) begin
+	if(~reset)begin
+		PC4_3 <= 32'd0;
+		ALUOut_3 <= 32'd0;
+		DatabusB_3 <= 32'd0;
+		Rt_3 <= 5'd0;
+		Rd_3 <= 5'd0;
+		RegDst_3 <= 2'd0;
+		MemToReg_3 <= 2'd0;
+		RegWr_3 <= 1'd0;
+		MemWr_3 <= 1'd0;
+		MemRd_3 <= 1'd0;
+	end
+	else begin
+		PC4_3 <= PC4_2;
+		ALUOut_3 <= ALUOut_EX;
+		DatabusB_3 <= DatabusB_2;
+		Rt_3 <= Rt_2;
+		Rd_3 <= Rd_2;
+		RegDst_3 <= RegDst_2;
+		MemToReg_3 <= MemToReg_2;
+		RegWr_3 <= RegWr_2;
+		MemWr_3 <= MemWr_2;
+		MemRd_3 <= MemRd_2;
+	end
+end
+
+//MEM
+	//Data Memory
+wire[31:0] rdata1;
+wire[31:0] rdata2;
+wire[31:0] ReadData;
+wire MemRd1,MemRd2,MemWr1,MemWr2;
+assign {MemRd2,MemRd1,MemWr2,MemWr1}=(ALUOut < 32'h40000000)?{MemRd_3,1'b0,MemWr_3,1'b0}:{1'b0,MemRd_3,1'b0,MemWr_3};
+Peripheral peripheral(reset,sysclk,clk,MemRd1,MemWr1,ALUOut_3,DatabusB_3,rdata1, led[7:0],switch,digi,timer, UART_RX,UART_TX,uart_send);
+DataMem data_memory(reset,clk,MemRd2,MemWr2,ALUOut_3,DatabusB_3,rdata2);
+assign ReadData = rdata1 | rdata2;
+
+always@(*)
+begin
+	case(MemToReg_3)
+		2'b00:DatabusC<=ALUOut_3;
+		2'b01:DatabusC<=ReadData;
+		default:DatabusC<=PC4_3;
+	endcase
+end
+
+always@(*)
+begin
+	case(RegDst_3)
+		2'b01:AddrC<=Rt_3;
+		2'b10:AddrC<=5'd31;
+		2'b11:AddrC<=5'd26;
+		default:AddrC<=Rd_3;
+	endcase
+end
+	//
+//MEM
 
 endmodule
